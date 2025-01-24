f854f6e01e861056c4dcfccb4756f95420599e6c
Jose Andres <andres.henao9@udea.edu.co>
2025-01-24 15:19:20 -0500

    El archivo main con las funcionalidades de personaje. Tambuén el .ui con el escenario.
    Signed-off-by: Jose Andres <andres.henao9@udea.edu.co>

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -6,9 +6,75 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    // Crear la escena
+    escena = new QGraphicsScene(this);
+
+    // Ajustar la escena y el Q Graphicsview
+
+    escena->setSceneRect(0, 0, ui->escenario->width() - 5, ui->escenario->height() - 5); // Ajustar los bordes de la escena
+
+    ui->escenario->setScene(escena);
+    temporizador= new QTimer;
+    connect(temporizador, &QTimer::timeout, this, [=]() {
+        Homero->set_posicion(); // Actualizar posición según la gravedad y el salto
+    });
+    temporizador->start(16);
+
+    temporizadorMovimiento = new QTimer(this);
+    connect(temporizadorMovimiento, &QTimer::timeout, this, &MainWindow::actualizarMovimiento);
+    temporizadorMovimiento->start(16); // Actualizar 60 veces por segundo (~16 ms)
+
+
+    // Crear el personaje y añadirlo a la escena
+    Homero = new Personaje(100, 100, 0, 0, 30, 350, ":/imagenes/caminar_sin_fondo.png", false);
+    escena->addItem(Homero);
+
+    // Ajustar posición inicial del personaje
+    Homero->setPos(30, 350); // Cambiar según dónde quieras que aparezca
+
+
+
+
+
+
 }
 
-MainWindow::~MainWindow()
-{
+MainWindow::~MainWindow() {
+    delete Homero;
+    delete escena;
+    delete temporizador;
+    delete temporizadorMovimiento;
     delete ui;
 }
+void MainWindow::keyPressEvent(QKeyEvent *i) {
+    teclasPresionadas.insert(i->key()); // Agregar la tecla presionada al conjunto
+}
+
+void MainWindow::keyReleaseEvent(QKeyEvent *i) {
+    teclasPresionadas.remove(i->key()); // Eliminar la tecla soltada del conjunto
+}
+
+
+
+
+void MainWindow::actualizarMovimiento() {
+    if (teclasPresionadas.contains(Qt::Key_D)) {
+        Homero->setX(Homero->x() + 10);
+        Homero->actualizar_pixmap(":/imagenes/caminar_sin_fondo.png");
+        QThread::msleep(10);
+        Homero->sprite_derecha();
+    }
+    if (teclasPresionadas.contains(Qt::Key_A)) {
+        Homero->setX(Homero->x() - 10);
+        Homero->actualizar_pixmap("://imagenes/caminar_izq_sin_fondo.png");
+        QThread::msleep(10);
+        Homero->sprite_derecha();
+    }
+    if (teclasPresionadas.contains(Qt::Key_Space)) {
+        Homero->saltar();
+    }
+}
+
+
+
--- mainwindow.h
+++ mainwindow.h
@@ -1,7 +1,12 @@
 #ifndef MAINWINDOW_H
 #define MAINWINDOW_H
 
+#include "personaje.h"
 #include <QMainWindow>
+#include <QGraphicsScene>
+#include <QKeyEvent>
+#include <QThread>
+#include <QTimer>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -9,15 +14,25 @@ class MainWindow;
 }
 QT_END_NAMESPACE
 
-class MainWindow : public QMainWindow
-{
+class MainWindow : public QMainWindow {
     Q_OBJECT
 
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+    void keyPressEvent(QKeyEvent *i);
+    void keyReleaseEvent(QKeyEvent *i);
+    void actualizarMovimiento();
 
 private:
     Ui::MainWindow *ui;
+    QGraphicsScene *escena;  // Escena para contener los elementos gráficos
+    QTimer *temporizador;
+    Personaje *Homero;       // Personaje principal
+private:
+    QSet<int> teclasPresionadas; // Para rastrear teclas activas
+    QTimer *temporizadorMovimiento; // Para manejar el movimiento continuo
+
 };
+
 #endif // MAINWINDOW_H
